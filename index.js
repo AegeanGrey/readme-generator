7e53c4328bfa106f287ed873414a773a6aa077c2
Aegean <todddharni@gmail.com>
2023-04-24 02:46:01 -0600

    Added questions for user to input

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// The 'questions' array contains a series of prompt questions for the user when called that the user must answer
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'How would you describe this project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How do you install this project?',
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'What are the guidlines for this project?',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'How do you use this project?',
+    },
+    {
+      type: 'input',
+      name: 'contact',
+      message: 'What is the best email to contact you regarding further questions?',
+    },
+  ];
+  
+  // The 'questions' array is then called and prompts the questions into the terminal awaiting user input
+  inquirer
+    .prompt(questions);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
